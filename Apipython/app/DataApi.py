11eaf8bc6bd80d959f0bcdad5cb8207f0b2b727c
Artur Mazur <94801659+itUrsus@users.noreply.github.com>
2022-06-17 20:58:41 +0200

    Update DataApi.py

--- Apipython/app/DataApi.py
+++ Apipython/app/DataApi.py
@@ -1,4 +1,6 @@
 import requests
+#from numpy.lib._datasource import Repository
+import sys
 from requests import get
 import db
 from sqlalchemy.orm import sessionmaker
@@ -8,161 +10,164 @@ from urllib.request import urlopen
 import db
 
 
-Session = sessionmaker(bind=db.engine)  # utworzenie sesji
+Session = sessionmaker(bind=db.engine)
 session = Session()
 
+class GitHubApi:
+    def __init__(self, repo_name, repo_owner, repo_url):
+        self.__repo_name = repo_name
+        self.__repo_owner = repo_owner
+        self.__repo_url = repo_url
 
-class Add_repo:
-    def __init__(self, id_repo, repo_name, repo_owner):
-        self.id_repo = id_repo
-        self.repo_name = repo_name
-        self.repo_owner = repo_owner
+    def get_repo(self):
+        repo_data = self.__get_repo_data()
+        commits = self.__get_commits()
+        branches = self.__get_branches()
+        pulls = self.__get_pull_requests()
+        files = self.__get_files()
 
+        return (repo_data, branches, commits, pulls, files)
 
-# zapisywanie outputu do bazy danych:
-def add_repo(id_repo, repo_name, repo_owner):
-    r = db.Repo(id_repo, repo_name, repo_owner)
-    session.add(r)
-    session.commit()
+    def __get_commits(self):
+        commits_data = self.__requests("commits")
 
+        commits = []
 
-res = requests.get("https://api.github.com/repos/stencila/test").json()
+        for data in commits_data:
 
-# print(res['id'], res['name'], res['owner']['login'])
-add_repo(res['id'], res['name'], res['owner']['login'])
+            commits.append(db.Commit(data['sha'], data['commit']['message'], data['commit']['author']['date'], data['commit']['author']['name'], data['commit']['author']['email']))
+        return commits
 
+    def __get_branches(self):
+        branches_data = self.__requests("branches")
 
-#
-#
+        branches = []
 
+        for data in branches_data:
+            branches.append(db.Branches(data['name'], data['commit']['sha']))
+        return branches
 
-def add_commit(sha, message, commited_date, author_login, author_name, author_email):
-    c = db.Commit(sha, message, commited_date, author_login, author_name, author_email)
-    session.add(c)
-    session.commit()
+    def __get_pull_requests(self):
+        pull_requests_data = self.__requests("pulls?state=all")
 
+        pulls = []
 
-with urlopen("https://api.github.com/repos/stencila/test/commits") as access_json:
-    read_content = json.load(access_json)
+        for data in pull_requests_data:
+            pulls.append(db.PullRequests(data['number'], data['state'], data['body'], data['commits_url']))
+        return pulls
 
-# replies_access = data['author']
-# print(replies_access)
+    def __get_files(self):
+        files_data = self.__requests("git/trees/master?recursive=1")
+        files = []
 
-for data in read_content:
-    sha = (data['sha'])
-    message = (data['commit']['message'])
-    commited_date = (data['commit']['author']['date'])
-    author_login = (data['author']['login'])
-    author_name = (data['commit']['author']['name'])
-    author_email = (data['commit']['author']['email'])
+        for data in files_data["tree"]:
+            files.append(db.FilesFromTree(data['path']))
 
-add_commit(sha, message, commited_date, author_login, author_name, author_email)
-
-
-# def add_files(filename, status, changes):
-#     f = db.Files(filename, status, changes)
-#     session.add(f)
-#     session.commit()
-#
-#
-# with urlopen(
-#         "https://api.github.com/repos/stencila/test/commits/22c0de5f9e2a4d53ae259187b16f6e9afbc53ded") as access_json:
-#     read_files = json.load(access_json)
-#
-# # zamiana słownika na listę:
-# for files in read_files['files']:
-#     filename = (files['filename'])
-#     status = (files['status'])
-#     changes = (files['changes'])
-#
-# add_files(filename, status, changes)
+        return files
 
 
-def add_filesFromtree(file):
-    tree = db.FilesFromTree(file)
-    session.add(tree)
-    session.commit()
+
+    def __get_repo_data(self):
+        repo_data = [self.__repo_name, self.__repo_owner]
 
 
-with urlopen(
-        "https://api.github.com/repos/stencila/test/git/trees/master?recursive=1") as access_json:
-    read_files = json.load(access_json)
-
-for file in read_files["tree"]:
-    file = file['path']
-    print(file)
-
-add_filesFromtree(file)
+        repo_data = repo_data.append(db.Repo(self.__repo_name, self.__repo_owner))
+        return repo_data
 
 
-def add_branches(name, sha):
-    b = db.Branches(name, sha)
-    session.add(b)
-    session.commit()
+    def __requests(self, endpoint: str = ""):
+        username = 'itUrsus'
+        token = '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'
+        #headers = ({'Authorization': 'access_token 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'})
+        response = requests.get(f"{self.__repo_url}/{self.__repo_owner}/{self.__repo_name}/{endpoint}", auth=(username,token)).json()
+        return response
+
+    def __str__(self):
+        return f"{gha.get_repo}"
 
 
-with urlopen("https://api.github.com/repos/stencila/test/branches") as access_json:
-    read_branches = json.load(access_json)
+class AddRepo(GitHubApi):
 
-# zamiana słownika na listę:
-for branches in read_branches:
-    name = (branches['name'])
-    sha = (branches['commit']['sha'])
+    def add_commit(self):
+        pass
 
-add_branches(name, sha)
+    def add_repo_data(self):
+        pass
 
+    def add_branches(self):
+        pass
 
-def add_pullrequests(number, state, body, commits_url):
-    pull = db.PullRequests(number, state, body, commits_url)
-    session.add(pull)
-    session.commit()
+    def add_files(self):
+        pass
 
-
-with urlopen("https://api.github.com/repos/stencila/test/pulls?state=all") as access_json:
-    read_pulls = json.load(access_json)
-
-for pulls in read_pulls:
-    number = (pulls['number'])
-    state = (pulls['state'])
-    body = (pulls['body'])
-    commits_url = (pulls['commits_url'])
-
-add_pullrequests(number, state, body, commits_url)
-
-#pobieranie danych z api
+    def add_pull_requests(self):
+        pass
 
 
-# class GetRepo:
-#     def __init__(self, repo_id, repo_name, repo_owner):
-#         self.repo_id = repo_id
-#         self.repo_name = repo_name
-#         self.repo_owner = repo_owner
-#
-# res = requests.get(
-# "https://api.github.com/repos/aapatre/Automatic-Udemy-Course-Enroller-GET-PAID-UDEMY-COURSES-for-FREE").json()
-#
-# g = (res['id'], res['name'], res['owner']['login'])
-#
-#
-# class AddRepo:
-#     def __init__(self, repo_id, repo_name, repo_owner):
-#
-# g = db.Repo(repo_id, repo_name, repo_owner)
-# session.add(g)
-# session.commit()
-#
-#
-# #AddRepo(res['id'], res['name'], res['owner']['login'])
-#
-#
-# class GetCommit:
+
+
+session.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class AddCommit:
 #     def __init__(self, sha, message, commited_date, author_login, author_name, author_email):
-#         self.sha = sha
-#         self.message = message
-#         self.commited_date = commited_date
-#         self.author_login = author_login
-#         self.author_name = author_name
-#         self.author_email = author_email
+#
+#         c = db.Commit(sha, message, commited_date, author_login, author_name, author_email)
+#         session.add(c)
+#         session.commit()
+#
+# AddCommit(sha, message, commited_date, author_login, author_name, author_email)
+
+
+# if __name__ == '__main__':
+#     gha = GitHubApi("My_first_repo", "itUrsus", "https://api.github.com/repos")
+#
+#     repo_data, branches, commits, pulls, files = gha.get_repo()
+#
+#     p = gha.get_repo()
+
+    # for d in p:
+    #     print(repo_data)
+
+
+
+
+
+
+
+
+# with urlopen("https://api.github.com/repos/stencila/test") as access_json:
+#      read_content = json.load(access_json)
+#
+# for data in read_content:
+#
+#     id_repo = ['id']
+#     repo_name = ['name']
+#     repo_owner = (['owner']['login'])
+#
+# print(data)
+
+
+#
+#
+# def add_commit(sha, message, commited_date, author_login, author_name, author_email):
+
+#     c = db.Commit(sha, message, commited_date, author_login, author_name, author_email)
+#     session.add(c)
+#     session.commit()
 #
 # with urlopen("https://api.github.com/repos/stencila/test/commits") as access_json:
 #     read_content = json.load(access_json)
@@ -175,22 +180,15 @@ add_pullrequests(number, state, body, commits_url)
 #     author_name = (data['commit']['author']['name'])
 #     author_email = (data['commit']['author']['email'])
 #
-# GetCommit(sha, message, commited_date, author_login, author_name, author_email)
+# add_commit(sha, message, commited_date, author_login, author_name, author_email)
+
 #
 #
-# class AddCommit:
-#     def __init__(self, sha, message, commited_date, author_login, author_name, author_email):
+# def add_files_from_tree(file):
+#     tree = db.FilesFromTree(file)
+#     session.add(tree)
+#     session.commit()
 #
-#         c = db.Commit(sha, message, commited_date, author_login, author_name, author_email)
-#         session.add(c)
-#         session.commit()
-#
-# AddCommit(sha, message, commited_date, author_login, author_name, author_email)
-#
-#
-# class GetFiles:
-#     def __init__(self, file):
-#         self.file = file
 #
 # with urlopen(
 #         "https://api.github.com/repos/stencila/test/git/trees/master?recursive=1") as access_json:
@@ -200,52 +198,32 @@ add_pullrequests(number, state, body, commits_url)
 #     file = file['path']
 #     print(file)
 #
-# GetFiles(file)
+# add_files_from_tree(file)
 #
 #
-# class AddFiles:
-#     def __init__(self, file):
+# def add_branches(name, sha):
+
+#     b = db.Branches(name, sha)
+#     session.add(b)
+#     session.commit()
 #
-#         tree = db.FilesFromTree(file)
-#         session.add(tree)
-#         session.commit()
-#
-# AddFiles(file)
-#
-#
-# class GetBranches:
-#     def __init__(self, name, sha):
-#         self.name = name
-#         self.sha = sha
 #
 # with urlopen("https://api.github.com/repos/stencila/test/branches") as access_json:
 #     read_branches = json.load(access_json)
 #
-# # zamiana słownika na listę:
 # for branches in read_branches:
-#      name = (branches['name'])
-#      sha = (branches['commit']['sha'])
+#     name = (branches['name'])
+#     sha = (branches['commit']['sha'])
 #
-# GetBranches(name, sha)
-#
-#
-# class AddBranches:
-#     def __init__(self, name, sha):
-#
-#         b = db.Branches(name, sha)
-#         session.add(b)
-#         session.commit()
+# add_branches(name, sha)
 #
 #
-# AddBranches(name, sha)
+# def add_pullrequests(number, state, body, commits_url):
+
+#     pull = db.PullRequests(number, state, body, commits_url)
+#     session.add(pull)
+#     session.commit()
 #
-#
-# # class GetPullRequests:
-# #     def __init__(self, number, state, body, commits_url):
-# #         self.number = number
-# #         self.state = state
-# #         self.body = body
-# #         self.commits_url = commits_url
 #
 # with urlopen("https://api.github.com/repos/stencila/test/pulls?state=all") as access_json:
 #     read_pulls = json.load(access_json)
@@ -256,40 +234,14 @@ add_pullrequests(number, state, body, commits_url)
 #     body = (pulls['body'])
 #     commits_url = (pulls['commits_url'])
 #
-# GetPullRequests(number, state, body, commits_url)
+# add_pullrequests(number, state, body, commits_url)
 #
 #
-# class AddPullRequests:
-#     def __init__(self, number, state, body, commits_url):
+# AddRepo(res['id'], res['name'], res['owner']['login'])
 #
-#         pull = db.PullRequests(number, state, body, commits_url)
-#         session.add(pull)
-#         session.commit()
+
+# # zamiana słownika na listę:
+# for branches in read_branches:
+#      name = (branches['name'])
+#      sha = (branches['commit']['sha'])
 #
-# GetPullRequests(number, state, body, commits_url)
-
-
-
-
-session.close()
-
-
-#
-#
-# # def add_files(filename, status, changes):
-# #      f = db.Files(filename, status, changes)
-# #      session.add(f)
-# #      session.commit()
-# #
-# #
-# # with urlopen("https://api.github.com/repos/stencila/test/commits/22c0de5f9e2a4d53ae259187b16f6e9afbc53ded") as access_json:
-# #     read_files = json.load(access_json)
-# #
-# #
-# # #zamiana słownika na listę:
-# # for files in read_files['files']:
-# #     filename = (files['filename'])
-# #     status = (files['status'])
-# #     changes = (files['changes'])
-# #
-# # add_files(filename, status, changes)
